99bd7655aafa19a6a6283ac00c961d1ac227e7c3
https://pahimar@github.com <pahimar@gmail.com>
2014-05-14 22:13:37 -0400

    Initial @Mod class, and fixing the package structure for the resources

---
+++ src/main/resources/com/teampapayamar/solstice/Solstice.java
@@ -0,0 +1,10 @@
+package com.teampapayamar.solstice;
+
+import cpw.mods.fml.common.Mod;
+
+@Mod(modid = "Solstice", name = "Solstice")
+public class Solstice
+{
+    @Mod.Instance
+    public static Solstice instance;
+}
