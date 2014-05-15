bd35bbbd2dd03e2c8d5c8eb59f8a85e4cb14ec43
https://pahimar@github.com <pahimar@gmail.com>
2014-05-14 23:52:11 -0400

    Auto Commit - Wed May 14 23:52:11 EDT 2014 (Bahamut)

--- src/main/resources/com/teampapayamar/solstice/init/ModItems.java
+++ src/main/resources/com/teampapayamar/solstice/init/ModItems.java
@@ -7,12 +7,14 @@ import net.minecraft.item.ItemArmor;
 
 public class ModItems
 {
-    public static final ItemArmor woolLinedIron = new ItemArmor(ItemArmor.ArmorMaterial.CLOTH, 0, 1);
+
+    public static final ItemArmorSolstice woolLinedIronHelmet = new ItemArmorSolstice(ItemArmor.ArmorMaterial.IRON, 0, 0);
+    public static final ItemArmorSolstice woolLinedIronChestPlate = new ItemArmorSolstice(ItemArmor.ArmorMaterial.IRON, 0, 1);
+    public static final ItemArmorSolstice woolLinedIronLeggings = new ItemArmorSolstice(ItemArmor.ArmorMaterial.IRON, 0, 2);
+    public static final ItemArmorSolstice woolLinedIronBoots = new ItemArmorSolstice(ItemArmor.ArmorMaterial.IRON, 0, 3);
 
     public static void init()
     {
-        woolLinedIron.setTextureName("wooliron_layer1").setCreativeTab(CreativeTabSolstice.SOLSTICE_TAB);
 
-        GameRegistry.registerItem(woolLinedIron, "item.wooliron");
     }
 }
