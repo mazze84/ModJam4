a0f0b84c92710e80db8bf1cbf7236700b8821e06
https://pahimar@github.com <pahimar@gmail.com>
2014-05-14 22:56:59 -0400

    Basic log helper, and basic configuration handler

--- src/main/resources/com/teampapayamar/solstice/configuration/ConfigurationHandler.java
+++ src/main/resources/com/teampapayamar/solstice/configuration/ConfigurationHandler.java
@@ -1,11 +1,29 @@
 package com.teampapayamar.solstice.configuration;
 
+import com.teampapayamar.solstice.util.LogHelper;
+import net.minecraftforge.common.config.Configuration;
+
 import java.io.File;
 
 public class ConfigurationHandler
 {
+    private static Configuration configuration;
+
     public static void init(File configFile)
     {
+        configuration = new Configuration(configFile);
 
+        try
+        {
+            configuration.load();
+        }
+        catch (Exception e)
+        {
+            LogHelper.error(e.getCause());
+        }
+        finally
+        {
+            configuration.save();
+        }
     }
 }
--- src/main/resources/com/teampapayamar/solstice/util/LogHelper.java
+++ src/main/resources/com/teampapayamar/solstice/util/LogHelper.java
@@ -1,5 +1,48 @@
 package com.teampapayamar.solstice.util;
 
+import com.teampapayamar.solstice.reference.Reference;
+import cpw.mods.fml.common.FMLLog;
+import org.apache.logging.log4j.Level;
+
 public class LogHelper
 {
+    public static void log(Level logLevel, Object object)
+    {
+        FMLLog.log(Reference.MOD_NAME, logLevel, "[Solstice] " + String.valueOf(object));
+    }
+
+    public static void all(Object object)
+    {
+        log(Level.ALL, object);
+    }
+
+    public static void debug(Object object)
+    {
+        log(Level.DEBUG, object);
+    }
+
+    public static void error(Object object)
+    {
+        log(Level.ERROR, object);
+    }
+
+    public static void fatal(Object object)
+    {
+        log(Level.FATAL, object);
+    }
+
+    public static void info(Object object)
+    {
+        log(Level.INFO, object);
+    }
+
+    public static void warn(Object object)
+    {
+        log(Level.WARN, object);
+    }
+
+    public static void trace(Object object)
+    {
+        log(Level.TRACE, object);
+    }
 }
