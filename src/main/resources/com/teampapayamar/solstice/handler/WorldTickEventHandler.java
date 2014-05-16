bcbd744ed611cd4b51ad291d9be71e49115cb630
https://pahimar@github.com <pahimar@gmail.com>
2014-05-15 21:36:10 -0400

    Re-jigging Chris' ModelCalendar that he made in Techne earlier (see previous commits) and some work on converting world time to player readable time

--- src/main/resources/com/teampapayamar/solstice/handler/WorldTickEventHandler.java
+++ src/main/resources/com/teampapayamar/solstice/handler/WorldTickEventHandler.java
@@ -1,6 +1,7 @@
 package com.teampapayamar.solstice.handler;
 
 import com.teampapayamar.solstice.util.LogHelper;
+import com.teampapayamar.solstice.util.TimeHelper;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import cpw.mods.fml.common.gameevent.TickEvent;
 
@@ -9,6 +10,6 @@ public class WorldTickEventHandler
     @SubscribeEvent
     public void onWorldTickEvent(TickEvent.WorldTickEvent event)
     {
-        LogHelper.info(event.world.getTotalWorldTime());
+        LogHelper.info(TimeHelper.convertTicksToTimeString(event.world.getWorldTime()));
     }
 }
--- src/main/resources/com/teampapayamar/solstice/util/TimeHelper.java
+++ src/main/resources/com/teampapayamar/solstice/util/TimeHelper.java
@@ -3,16 +3,31 @@ package com.teampapayamar.solstice.util;
 public class TimeHelper
 {
     /**
-     *  Scratch notes because I (Pahimar) am lazy
-     *  - 24,000 ticks in a MC day
-     *  - 168,000 ticks in a MC week
-     *  - 672,000 ticks in a MC month
-     *  - 2,016,000 ticks in a MC season
-     *  - 8,064,000 ticks in a MC year
+     * Scratch notes because I (Pahimar) am lazy - 24,000 ticks in a MC day == 20 minutes game time - 168,000 ticks in a
+     * MC week == 140 minutes game time == 2 hours 20 minutes - 672,000 ticks in a MC month == 9 hours 20 minutes -
+     * 2,016,000 ticks in a MC season - 8,064,000 ticks in a MC year
      */
 
     public static String convertTicksToTimeString(long worldTime)
     {
-        return "";
+        long tempTime = worldTime + 2016000L;
+        long year = tempTime / 8064000L;
+
+        tempTime = tempTime % 8064000L;
+        long season = tempTime / 2016000L;
+
+        tempTime = tempTime % 2016000L;
+        long month = tempTime / 672000L;
+
+        tempTime = tempTime % 672000L;
+        long week = tempTime / 168000L;
+
+        tempTime = tempTime % 168000L;
+        long day = tempTime / 24000;
+
+        tempTime = tempTime % 24000;
+        long timeOfDay = tempTime;
+
+        return String.format("Year: %s, Season: %s, Month: %s, Week: %s, Day: %s, Time of Day: %s", year, season, month, week, day, timeOfDay);
     }
 }
