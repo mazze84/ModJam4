d8e4fee8bc9a0ccda5481965f54986735518c817
https://pahimar@github.com <pahimar@gmail.com>
2014-05-15 22:10:44 -0400

    Auto Commit - Thu May 15 22:10:44 EDT 2014 (Bahamut)

--- src/main/resources/com/teampapayamar/solstice/proxy/ServerProxy.java
+++ src/main/resources/com/teampapayamar/solstice/proxy/ServerProxy.java
@@ -1,5 +1,9 @@
 package com.teampapayamar.solstice.proxy;
 
+import com.teampapayamar.solstice.handler.WorldTickEventHandler;
+import cpw.mods.fml.common.FMLCommonHandler;
+
 public class ServerProxy extends CommonProxy
 {
+
 }
--- src/main/resources/com/teampapayamar/solstice/util/TimeHelper.java
+++ src/main/resources/com/teampapayamar/solstice/util/TimeHelper.java
@@ -7,13 +7,12 @@ public class TimeHelper
      * MC week == 140 minutes game time == 2 hours 20 minutes - 672,000 ticks in a MC month == 9 hours 20 minutes -
      * 2,016,000 ticks in a MC season - 8,064,000 ticks in a MC year
      */
-
-    public static final long TICKS_IN_DAY = 24000L;
+    public static final long TICKS_IN_HOUR = 1000L;
+    public static final long TICKS_IN_DAY = TICKS_IN_HOUR * 24;
     public static final long TICKS_IN_WEEK = TICKS_IN_DAY * 7;
     public static final long TICKS_IN_MONTH = TICKS_IN_WEEK * 4;
     public static final long TICKS_IN_SEASON = TICKS_IN_MONTH * 3;
     public static final long TICKS_IN_YEAR = TICKS_IN_SEASON * 4;
-
 
     public static String convertTicksToTimeString(long worldTime)
     {
@@ -24,12 +23,14 @@ public class TimeHelper
         long season = (adjustedWorldTime % TICKS_IN_YEAR) / TICKS_IN_SEASON;
         long month = (adjustedWorldTime % TICKS_IN_YEAR) / TICKS_IN_MONTH;
         long week_of_year = (adjustedWorldTime % TICKS_IN_YEAR) / TICKS_IN_WEEK;
-        long week_of_month = (adjustedWorldTime % TICKS_IN_YEAR) / (month * 4 * TICKS_IN_WEEK);
+        long week_of_month = (adjustedWorldTime % TICKS_IN_YEAR % TICKS_IN_MONTH) / TICKS_IN_WEEK;
         long day_of_year = (adjustedWorldTime % TICKS_IN_YEAR) / TICKS_IN_DAY;
-        long day_of_month =
+        long day_of_month = (adjustedWorldTime % TICKS_IN_YEAR % TICKS_IN_MONTH) / TICKS_IN_DAY;
+        long day_of_week = (adjustedWorldTime % TICKS_IN_YEAR % TICKS_IN_MONTH % TICKS_IN_WEEK) / TICKS_IN_DAY;
+        long hour_of_day = (adjustedWorldTime % TICKS_IN_YEAR % TICKS_IN_MONTH % TICKS_IN_WEEK % TICKS_IN_DAY) / TICKS_IN_HOUR;
 
-        long timeOfDay = adjustedWorldTime;
+        // ARGH BRAIN, WHY WONT YOU BRAIN
 
-        return String.format("Year: %s, Season: %s, Month: %s, Week of Year: %s, Week of Month: %s, Day of Year: %s, Time of Day: %s", year, season, month, week_of_year, week_of_month, day_of_year, timeOfDay);
+        return String.format("Year: %s, Season: %s, Month: %s, Week of Year: %s, Week of Month: %s, Day of Year: %s, Day of Month, %s, Day of Week: %s, Hour of Day: %s", year, season, month, week_of_year, week_of_month, day_of_year, day_of_month, day_of_week, hour_of_day);
     }
 }
