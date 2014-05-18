fe08536ec087c8c578ef8df126761b818484a836
pahimar <pahimar@gmail.com>
2014-05-18 18:45:00 -0400

    Auto Commit - Sun May 18 18:45:00 EDT 2014 (Bahamut)

--- src/main/java/com/teampapayamar/solstice/handler/PlayerInteractEventHandler.java
+++ src/main/java/com/teampapayamar/solstice/handler/PlayerInteractEventHandler.java
@@ -2,14 +2,27 @@ package com.teampapayamar.solstice.handler;
 
 import com.teampapayamar.solstice.util.LogHelper;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.event.entity.player.PlayerUseItemEvent;
+import net.minecraftforge.oredict.OreDictionary;
 
 public class PlayerInteractEventHandler
 {
     @SubscribeEvent
     public void onPlayerInteractEvent(PlayerInteractEvent event)
     {
-        LogHelper.info(event.useItem);
+        if (!event.entityPlayer.getEntityWorld().isRemote)
+        {
+            if (event.action == PlayerInteractEvent.Action.RIGHT_CLICK_BLOCK)
+            {
+                int oreId = OreDictionary.getOreID(event.entityPlayer.getCurrentEquippedItem());
+
+                if ((oreId != -1) && OreDictionary.getOreName(oreId).equals("stickWood") && event.entityPlayer.getCurrentEquippedItem().stackSize >= 6)
+                {
+
+                }
+            }
+        }
     }
 }
