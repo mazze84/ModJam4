d27aa101e9f397c1525e40ff2312794083249d0e
pahimar <pahimar@gmail.com>
2014-05-18 19:21:06 -0400

    Auto Commit - Sun May 18 19:21:06 EDT 2014 (Bahamut)

--- src/main/java/com/teampapayamar/solstice/handler/PlayerInteractEventHandler.java
+++ src/main/java/com/teampapayamar/solstice/handler/PlayerInteractEventHandler.java
@@ -1,5 +1,6 @@
 package com.teampapayamar.solstice.handler;
 
+import com.teampapayamar.solstice.init.ModBlocks;
 import com.teampapayamar.solstice.util.LogHelper;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.event.entity.player.PlayerEvent;
@@ -20,7 +21,8 @@ public class PlayerInteractEventHandler
 
                 if ((oreId != -1) && OreDictionary.getOreName(oreId).equals("stickWood") && event.entityPlayer.getCurrentEquippedItem().stackSize >= 6)
                 {
-
+                    event.entityPlayer.getCurrentEquippedItem().stackSize = event.entityPlayer.getCurrentEquippedItem().stackSize - 6;
+                    event.entityPlayer.getEntityWorld().setBlock(event.x, event.y + 1, event.z, ModBlocks.campFire);
                 }
             }
         }
