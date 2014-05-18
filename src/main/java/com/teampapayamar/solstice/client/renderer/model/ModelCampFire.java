2fb1be74615a948fdb89d794332e387b8c597a21
pahimar <pahimar@gmail.com>
2014-05-18 16:51:45 -0400

    Auto Commit - Sun May 18 16:51:45 EDT 2014 (Bahamut)

--- src/main/java/com/teampapayamar/solstice/client/renderer/model/ModelCampFire.java
+++ src/main/java/com/teampapayamar/solstice/client/renderer/model/ModelCampFire.java
@@ -6,72 +6,65 @@ import net.minecraft.entity.Entity;
 
 public class ModelCampFire extends ModelBase
 {
-    //fields
-    ModelRenderer Shape1;
-    ModelRenderer Shape2;
-    ModelRenderer Shape3;
-    ModelRenderer Shape4;
-    ModelRenderer Shape5;
-    ModelRenderer Shape6;
+    ModelRenderer shape1;
+    ModelRenderer shape2;
+    ModelRenderer shape3;
+    ModelRenderer shape4;
+    ModelRenderer shape5;
+    ModelRenderer shape6;
 
     public ModelCampFire()
     {
         textureWidth = 64;
-        textureHeight = 32;
+        textureHeight = 64;
 
-        Shape1 = new ModelRenderer(this, 0, 0);
-        Shape1.addBox(0F, 0F, 0F, 1, 1, 12);
-        Shape1.setRotationPoint(0F, 23F, -7F);
-        Shape1.setTextureSize(64, 32);
-        Shape1.mirror = true;
-        setRotation(Shape1, 0.5948578F, 0F, 0F);
+        shape1 = new ModelRenderer(this, 0, 0);
+        shape1.addBox(0F, 0F, 0F, 1, 1, 12);
+        shape1.setRotationPoint(0F, 23F, -7F);
+        shape1.setTextureSize(64, 32);
+        setRotation(shape1, 0.5948578F, 0F, 0F);
 
-        Shape2 = new ModelRenderer(this, 0, 0);
-        Shape2.addBox(0F, 0F, 0F, 1, 1, 12);
-        Shape2.setRotationPoint(-7F, 23F, 0F);
-        Shape2.setTextureSize(64, 32);
-        Shape2.mirror = true;
-        setRotation(Shape2, 0.5934119F, 1.658063F, 0F);
+        shape2 = new ModelRenderer(this, 0, 0);
+        shape2.addBox(0F, 0F, 0F, 1, 1, 12);
+        shape2.setRotationPoint(-7F, 23F, 0F);
+        shape2.setTextureSize(64, 32);
+        setRotation(shape2, 0.5934119F, 1.658063F, 0F);
 
-        Shape3 = new ModelRenderer(this, 0, 0);
-        Shape3.addBox(0F, 0F, 0F, 1, 1, 12);
-        Shape3.setRotationPoint(0F, 23F, 7F);
-        Shape3.setTextureSize(64, 32);
-        Shape3.mirror = true;
-        setRotation(Shape3, 0.5934119F, 3.141593F, 0F);
+        shape3 = new ModelRenderer(this, 0, 0);
+        shape3.addBox(0F, 0F, 0F, 1, 1, 12);
+        shape3.setRotationPoint(0F, 23F, 7F);
+        shape3.setTextureSize(64, 32);
+        setRotation(shape3, 0.5934119F, 3.141593F, 0F);
 
-        Shape4 = new ModelRenderer(this, 0, 0);
-        Shape4.addBox(0F, 0F, 0F, 1, 1, 12);
-        Shape4.setRotationPoint(7F, 23F, 4F);
-        Shape4.setTextureSize(64, 32);
-        Shape4.mirror = true;
-        setRotation(Shape4, 0.5934119F, -2.193538F, 0F);
+        shape4 = new ModelRenderer(this, 0, 0);
+        shape4.addBox(0F, 0F, 0F, 1, 1, 12);
+        shape4.setRotationPoint(7F, 23F, 4F);
+        shape4.setTextureSize(64, 32);
+        setRotation(shape4, 0.5934119F, -2.193538F, 0F);
 
-        Shape5 = new ModelRenderer(this, 0, 0);
-        Shape5.addBox(0F, 0F, 0F, 1, 1, 12);
-        Shape5.setRotationPoint(5F, 23F, -6F);
-        Shape5.setTextureSize(64, 32);
-        Shape5.mirror = true;
-        setRotation(Shape5, 0.5934119F, -0.6878043F, 0F);
+        shape5 = new ModelRenderer(this, 0, 0);
+        shape5.addBox(0F, 0F, 0F, 1, 1, 12);
+        shape5.setRotationPoint(5F, 23F, -6F);
+        shape5.setTextureSize(64, 32);
+        setRotation(shape5, 0.5934119F, -0.6878043F, 0F);
 
-        Shape6 = new ModelRenderer(this, 0, 0);
-        Shape6.addBox(0F, 0F, 0F, 1, 1, 12);
-        Shape6.setRotationPoint(-5F, 23F, 6F);
-        Shape6.setTextureSize(64, 32);
-        Shape6.mirror = true;
-        setRotation(Shape6, 0.5934119F, 2.435199F, 0F);
+        shape6 = new ModelRenderer(this, 0, 0);
+        shape6.addBox(0F, 0F, 0F, 1, 1, 12);
+        shape6.setRotationPoint(-5F, 23F, 6F);
+        shape6.setTextureSize(64, 32);
+        setRotation(shape6, 0.5934119F, 2.435199F, 0F);
     }
 
     public void render(Entity entity, float f, float f1, float f2, float f3, float f4, float f5)
     {
         super.render(entity, f, f1, f2, f3, f4, f5);
         setRotationAngles(f, f1, f2, f3, f4, f5, entity);
-        Shape1.render(f5);
-        Shape2.render(f5);
-        Shape3.render(f5);
-        Shape4.render(f5);
-        Shape5.render(f5);
-        Shape6.render(f5);
+        shape1.render(f5);
+        shape2.render(f5);
+        shape3.render(f5);
+        shape4.render(f5);
+        shape5.render(f5);
+        shape6.render(f5);
     }
 
     private void setRotation(ModelRenderer model, float x, float y, float z)
