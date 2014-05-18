7e704ae571a2cc65c4979f433e31f54f386ec7a4
https://pahimar@github.com <pahimar@gmail.com>
2014-05-18 11:00:05 -0400

    Auto Commit - Sun May 18 11:00:05 EDT 2014 (Bahamut)

--- src/main/resources/com/teampapayamar/solstice/tileentity/TileEntityCalendar.java
+++ src/main/resources/com/teampapayamar/solstice/tileentity/TileEntityCalendar.java
@@ -1,9 +1,26 @@
 package com.teampapayamar.solstice.tileentity;
 
 import net.minecraft.block.ITileEntityProvider;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.world.World;
 
 public class TileEntityCalendar extends TileEntitySolstice
 {
+    public TileEntityCalendar()
+    {
+        super();
+    }
+
+    @Override
+    public void readFromNBT(NBTTagCompound nbtTagCompound)
+    {
+        super.readFromNBT(nbtTagCompound);
+    }
+
+    @Override
+    public void writeToNBT(NBTTagCompound nbtTagCompound)
+    {
+        super.writeToNBT(nbtTagCompound);
+    }
 }
